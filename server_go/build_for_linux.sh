41aa5fc569b97545774d110ea910d3f57f807096
kfrime <kfrime@163.com>
2019-10-03 13:53:57 +0800

    change excute file name of golang

--- server_go/build_for_linux.sh
+++ server_go/build_for_linux.sh
@@ -5,4 +5,5 @@ cur=`pwd`
 # export GOPATH=$GOPATH:${cur}
 export GOPATH=~/go:${cur}
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o yonder_server_go main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server_go main.go
+
