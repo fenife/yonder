ff3032ebd72cab76af68af0020144d21542b6fb7
kfrime <kfrime@163.com>
2019-12-03 21:45:28 +0800

    add http status msg

--- server_py3/starry/response.py
+++ server_py3/starry/response.py
@@ -15,12 +15,21 @@ class Response(object):
     # todo: why this line cause content-length error ?
     # default_headers = [('Content-Type', 'text/json')]
 
-    def __init__(self, data=None, code=None, headers=None):
+    def __init__(self, data=None, code=None, msg=None, headers=None):
         self.data = data
         self.status_code = code or self.default_status_code
+        self._msg = msg
         self.mimetype = self.default_mimetype
         self.headers = headers or [('Content-Type', 'text/json')]
         self._cookies = SimpleCookie()
+
+    @property
+    def msg(self):
+        if self._msg:
+            return self._msg
+
+        n = code2name(self.status_code)
+        return n
 
     @property
     def name(self):
@@ -48,14 +57,14 @@ class Response(object):
             # 返回的格式是固定的，bytes需要手动拼接
             c = b'{"code":' + str(self.status_code).encode(self.charset) + b','
             d = b'"data":"' + self.data + b'",'
-            m = b'"msg":"' + self.name.encode(self.charset) + b'"}'
+            m = b'"msg":"' + self.msg.encode(self.charset) + b'"}'
             body = c + d + m
             return body
 
         body = {
             "code": self.status_code,
             "data": {} if self.data is None else self.data,
-            "msg": self.name,
+            "msg": self.msg,
         }
         # self.data = ujson.dumps(self.data).encode(self.charset)
         body = ujson.dumps(body).encode(self.charset)
@@ -68,5 +77,5 @@ class Response(object):
         headers = self._get_headers()
         start_response(status, headers)
 
-        print('body len:', len(body))
+        # print('body len:', len(body))
         return [body]
