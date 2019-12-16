d28356809d3c826b572cf440859ff942f3731dd6
kfrime <kfrime@163.com>
2019-12-16 21:58:53 +0800

    add api: update article

--- server_py3/web/web/api/articles.py
+++ server_py3/web/web/api/articles.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
+import hashlib
 from sim.exceptions import abort
+from sim.response import Response
 from .. import app, db, cache_pool
 from ..model import User, Category, Article
 from ..consts import RespCode, Permission, RoleUser, RoleAdmin, Roles, USER, CATEGORY, ARTICLE
 from ..decorators import permission_required, login_required
+
+
+def content_hash(content):
+    """计算文章内容的hash，用于对比更新前后内容是否有改变"""
+    if not content:
+        return
+
+    m = hashlib.md5()
+    m.update(content.encode('utf-8'))
+    h = m.hexdigest()
+    app.logger.debug(h)
+    return h
 
 
 @app.route('/api/article', methods=('POST', ))
@@ -72,7 +86,79 @@ def article_create(ctx):
 @app.route('/api/article/:aid', methods=('PUT', ))
 @permission_required(Permission.admin)
 def article_update(ctx):
-    return
+    aid = ctx.request.get_param('aid')
+    try:
+        aid = int(aid)
+    except Exception as e:
+        app.logger.error(f"aid must be an integer, but get: {aid}")
+        abort(RespCode.error, "aid must be an integer")
+
+    input_json = ctx.request.json()
+    if not input_json:
+        abort(RespCode.error, "body is empty")
+
+    supported = {'cate_id', 'title', 'content'}
+    unsupported = set(input_json.keys()) - supported
+    if unsupported:
+        abort(RespCode.error, f"unsupported fields: {list(unsupported)}")
+
+    article = Article.find(aid)
+    if not article:
+        abort(RespCode.error, "article not found")
+
+    # get user
+    user = ctx.user
+    assert isinstance(user, User)
+
+    need_update = False
+    if 'cate_id' in input_json:
+        cate_id = input_json.pop('cate_id')
+
+        # categroy id need update
+        if cate_id != article.cate_id:
+
+            if Category.find(cate_id) is None:
+                abort(RespCode.error, f"category not existed")
+
+            article.cate_id = cate_id
+            need_update = True
+
+    if 'title' in input_json:
+        new_title = input_json.pop('title')
+
+        # title need update
+        if new_title != article.title:
+            # prevent xss of title
+            new_title = Article.valid_title(new_title)
+
+            # if new title existed
+            old = Article.find_by_title(new_title)
+            if old:
+                abort(RespCode.error, f"title existed")
+
+            article.title = new_title
+            need_update = True
+
+    if 'content' in input_json:
+        new_content = input_json.pop('content')
+
+        # content need update
+        if content_hash(new_content) != content_hash(article.content):
+            article.content = new_content
+            need_update = True
+
+    if need_update:
+        if article.modify() is False:
+            abort(RespCode.error, "update article error")
+
+        return article
+
+    else:
+        return Response(
+            data=article,
+            code=200,
+            msg="article is the same as before, no need to update"
+        )
 
 
 @app.route('/api/article', methods=('GET', ))
