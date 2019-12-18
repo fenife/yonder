33e57093803c19dac11bd03ed0c3d727ce90a069
kfrime <kfrime@163.com>
2019-12-18 23:15:11 +0800

    add api: about

---
+++ server_py3/web/web/api/about.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import hashlib
+from sim.exceptions import abort
+from sim.response import Response
+from sim.context import AppRequestContext
+from .. import app, db, cache_pool
+from ..model import User, Category, Article
+from ..consts import RespCode, Permission, RoleUser, RoleAdmin, Roles, USER, CATEGORY, ARTICLE
+from ..decorators import permission_required, login_required
+
+
+@app.route('/api/about')
+def about(ctx: AppRequestContext):
+    article = Article.find_by_title('about')
+    if not article:
+        abort(RespCode.error, "about not found")
+
+    article = Article.get_with_more_detail(article.id)
+    return article
