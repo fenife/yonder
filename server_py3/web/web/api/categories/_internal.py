ba6986bba017ac54ded06a1f81d701ee81904683
kfrime <kfrime@163.com>
2020-01-12 11:22:34 +0800

    refactor api: category update; add desc

---
+++ server_py3/web/web/api/categories/_internal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from sim.context import AppRequestContext
+from sim.exceptions import abort
+from ...consts import RespCode
+from .._utils import to_int
+
+
+def get_cid_from_request(ctx: AppRequestContext):
+    # cid: category id
+    cid = ctx.request.get_param('cid')      # 动态路由参数
+    if not cid:
+        cid = ctx.request.get_uri_arg('cid')        # url中?后面的参数
+
+    cid = to_int('cid', cid)
+
+    return cid
