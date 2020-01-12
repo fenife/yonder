9df51add3d0c68ad12211bb1dda77dcc21682527
kfrime <kfrime@163.com>
2020-01-12 15:30:41 +0800

    add file logger in dev

--- server_py3/web/web/config/__init__.py
+++ server_py3/web/web/config/__init__.py
@@ -15,6 +15,17 @@ class BaseConfig(object):
     def init_app(cls, app: Application):
         app.update_config(cls.configs)
 
+        # 写日志到文件中
+        from logging.handlers import TimedRotatingFileHandler
+        from sim.log import formatter
+
+        fn = cls.configs['LOG_FILE']
+        fh = TimedRotatingFileHandler(filename=fn, when='MIDNIGHT')
+        fh.setFormatter(formatter)
+        app.logger.addHandler(fh)
+
+        app.logger.setLevel(cls.log_level)
+
 
 class DevConfig(BaseConfig):
     """开发环境的配置"""
@@ -26,21 +37,6 @@ class LiveConfig(BaseConfig):
     """线上环境的配置"""
     configs = live_configs
     log_level = logging.INFO
-
-    @classmethod
-    def init_app(cls, app: Application):
-        app.update_config(cls.configs)
-
-        # 写日志到文件中
-        from logging.handlers import TimedRotatingFileHandler
-        from sim.log import formatter
-
-        fn = live_configs['LOG_FILE']
-        fh = TimedRotatingFileHandler(filename=fn, when='MIDNIGHT')
-        fh.setFormatter(formatter)
-        app.logger.addHandler(fh)
-
-        app.logger.setLevel(cls.log_level)
 
 
 config = {
