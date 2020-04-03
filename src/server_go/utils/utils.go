027b4861e053a1c9faa829d9bf9a93c2a53053b7
kfrime <kfrime@163.com>
2020-04-03 21:46:51 +0800

    add: deepcopy func

--- src/server_go/utils/utils.go
+++ src/server_go/utils/utils.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 func PrettyPrint(v interface{}) {
@@ -12,4 +15,24 @@ func PrettyPrint(v interface{}) {
 	} else {
 		fmt.Println(string(b))
 	}
+}
+
+
+// Clone deep-copies src to dst
+// src, dst must be pointer
+func Deepcopy(src, dst interface{}) error {
+
+	//buff := new(bytes.Buffer)
+	var buff bytes.Buffer
+	enc := gob.NewEncoder(&buff)
+	dec := gob.NewDecoder(&buff)
+	if err := enc.Encode(src); err != nil {
+		log.Print(err)
+		return err
+	}
+	if err := dec.Decode(dst); err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
 }
