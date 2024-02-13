98f9c3e3608eba8bebadd70982aae815fdb8a1f1
fenife <fenife@163.com>
2024-02-13 13:23:00 +0800

    add: ping handler

---
+++ server_go/controller/handler/ping_handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"server-go/utils/renderx"
+)
+
+type PingHandler struct{}
+
+func NewPingHandler() *PingHandler {
+	return &PingHandler{}
+}
+
+// Ping godoc
+// @Summary      ping,检查服务是否正常
+// @Tags         ping
+// @Produce      json
+// @Success      200  {object}  renderx.Response
+// @Router       /ping [get]
+func (ctrl *PingHandler) Ping(c *gin.Context) {
+	renderx.SuccOutput(c)
+}
