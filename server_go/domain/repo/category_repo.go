adf700e53d5bfb62540573b334173f89d531b8a5
fenife <fenife@163.com>
2024-03-24 09:00:32 +0800

    add: create category in repo

--- server_go/domain/repo/category_repo.go
+++ server_go/domain/repo/category_repo.go
@@ -8,4 +8,6 @@ import (
 type ICategoryRepo interface {
 	GetCategoryList(ctx context.Context) ([]entity.Category, error)
 	FindById(ctx context.Context, cateId uint64) (*entity.Category, error)
+	FindByName(ctx context.Context, name string) (*entity.Category, error)
+	Create(ctx context.Context, cate *entity.Category) error
 }
--- server_go/infra/persistence/category_repo.go
+++ server_go/infra/persistence/category_repo.go
@@ -34,3 +34,17 @@ func (r *CategoryRepo) FindById(ctx context.Context, cateId uint64) (*entity.Cat
 	}
 	return &cate, err
 }
+
+func (r *CategoryRepo) FindByName(ctx context.Context, name string) (*entity.Category, error) {
+	var cate entity.Category
+	err := r.db.WithContext(ctx).Where("name = ?", name).First(&cate).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		// 通过ID判断是否存在
+		return &cate, nil
+	}
+	return &cate, err
+}
+
+func (r *CategoryRepo) Create(ctx context.Context, cate *entity.Category) error {
+	return r.db.WithContext(ctx).Create(cate).Error
+}
